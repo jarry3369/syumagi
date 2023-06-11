737533eccbe02aa27a65fd7a09f13f561ce2ae7d
jarry3369 <jarry3369@gmail.com>
2023-06-12 00:54:54 +0900

    add gnb layout

---
+++ src/layout/gnb_layout.tsx
@@ -0,0 +1,38 @@
+import NavigationBar from "@/components/NavigationBar";
+import { Fragment } from "react";
+import { Outlet } from "react-router-dom";
+
+import { TNavContent } from "@/constants/types";
+import Stack from "@/components/Stack";
+
+const nav_contents: Array<TNavContent> = [
+  {
+    path: "/",
+    label: "Home",
+  },
+  {
+    path: "/tweets",
+    label: "Tweets",
+  },
+  {
+    path: "/notice",
+    label: "notice",
+  },
+];
+
+const GnbLayout = () => {
+  return (
+    <Fragment>
+      <NavigationBar items={nav_contents} />
+      <Stack
+        justify="center"
+        padding={"84px 0 0 "}
+        sx={{ overflow: "scroll", height: "calc(100vh - 84px)" }}
+      >
+        <Outlet />
+      </Stack>
+    </Fragment>
+  );
+};
+
+export default GnbLayout;
