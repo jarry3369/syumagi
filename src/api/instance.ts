4a9caa14905e6815db644503820ed5a3ab0dd3dd
jarry3369 <jarry3369@gmail.com>
2023-06-12 00:47:47 +0900

    instance부 수정

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -3,19 +3,22 @@ import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 const CancelToken = axios.CancelToken;
 export let canceler: any;
 
-export const baseAPI = axios.create({
+const baseAPI = axios.create({
   baseURL: import.meta.env.VITE_SERVER_URL,
 });
 
-export const request = ({ ...options }: AxiosRequestConfig) => {
+export const request = async ({ ...options }: AxiosRequestConfig) => {
   baseAPI.defaults.headers.common.token =
     window.localStorage.getItem("accessToken") || "";
-  const onSuccess = (response: AxiosResponse) => response.data;
+  const onSuccess = (response: AxiosResponse) => {
+    return response.data;
+  };
   const onError = (error: any) => {
+    console.error(error);
     return null;
   };
 
-  return baseAPI({
+  return await baseAPI({
     ...options,
     cancelToken: new CancelToken(function executor(c) {
       // An executor function receives a cancel function as a parameter
