a0e487342fd9e3eced3cd099d23afabdace83b79
jarry3369 <jarry3369@gmail.com>
2023-06-12 00:46:48 +0900

    라우터 구조 크게 수정

--- src/ConfigRouters.tsx
+++ src/ConfigRouters.tsx
@@ -1,11 +1,33 @@
 import { useRoutes } from "react-router-dom";
-import { Home, Notice, Feed } from "@/pages";
+import { Home, Notice, Tweets, NotFound, Loading } from "@/pages";
+import GnbLayout from "./layout/gnb_layout";
+import { Suspense } from "react";
 
 const ConfigRouters = () => {
   const element = useRoutes([
-    { path: "/", element: <Home /> },
-    { path: "/notice/", element: <Notice /> },
-    { path: "/feed/", element: <Feed /> },
+    {
+      element: <GnbLayout />,
+      children: [
+        {
+          path: "/",
+          element: (
+            <Suspense fallback={<Loading />}>
+              <Home />
+            </Suspense>
+          ),
+        },
+        {
+          path: "/notice/",
+          element: <Notice />,
+        },
+        {
+          path: "/tweets/",
+          element: <Tweets />,
+        },
+      ],
+    },
+
+    { path: "*", element: <NotFound /> },
   ]);
 
   return element;
