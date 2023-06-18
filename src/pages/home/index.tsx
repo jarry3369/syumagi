c9c6e93339e2ed65875753632a160d16184f16ed
jarry3369 <jarry3369@gmail.com>
2023-06-19 00:41:27 +0900

    /Home 랜딩으로 albumSlider 추가

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,23 +1,7 @@
-import useSWR from "swr";
-
-import { getMusics } from "@/api";
-import Stack from "@/components/Stack";
-import { Text } from "@/components/Typography";
+import AlbumSlider from "@/components/AlbumSlider";
 
 export const Home = () => {
-  const { data } = useSWR("getMusics", getMusics, {
-    onSuccess(res) {
-      console.log(res);
-    },
-  });
-  return (
-    <Stack>
-      {data?.map((d: any) => (
-        <Text key={d.name} styleName="subtitle">
-          {d.name}
-        </Text>
-      ))}
-    </Stack>
-  );
+  return <AlbumSlider />;
 };
+
 export default Home;
