d674243b7699b6e739f32b1559a6e8cbfe35c952
jarry3369 <jarry3369@gmail.com>
2023-06-19 00:39:42 +0900

    getAlbums api 추가

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,3 +3,14 @@ import { request } from "@/api/instance";
 export const getMusics = (): Promise<any> => {
   return request({ url: `/musics` });
 };
+
+export const getAlbums = (): Promise<
+  Array<{
+    cover: string;
+    releaseDate: string;
+    title: string;
+    type: string;
+  }>
+> => {
+  return request({ url: `/albums` });
+};
